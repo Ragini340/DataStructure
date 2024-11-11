84b2f0d0f73dd3860eb7cfe7a19ab92a614723e4
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2024-11-11 11:37:56 +0530

    Add files via upload

---
+++ InsertionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class InsertionSort
+    {
+        static void insertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            for (int k = 0; k < arr.Length; k++)
+            {
+                Console.WriteLine(arr[k] + " ");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 2, 6, 8, 10, 9, 5 };
+            insertionSort(arr);
+        }
+    }
+}
